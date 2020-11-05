a58328f7d4ccb6e565e9ac6746fc150b98e7f03e
Brian Demers <bdemers@apache.org>
2020-11-05 10:13:15 -0500

    initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ComponentScan(basePackages = {"com.example.demo.*"})
+@RequestMapping(value = {"/demo"})
+public class DemoController {
+
+    @RequestMapping(value = "/data", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+    public ResponseEntity<String> getDemoData(OAuth2Authentication user) {
+        return new ResponseEntity<String>("Processed Request", HttpStatus.OK);
+    }
+}
