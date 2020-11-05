f3f4b1684117a288030e4e69cdc6c4b065c2b050
Brian Demers <bdemers@apache.org>
2020-11-05 10:37:34 -0500

    Remove dependency conflicts with old spring-security-oauth-autoconfig dependencies
    * This removes the need for @EnableResourceServer annotation
    * And changes the Object injected into you controller,
    * `okta.oauth2.clientId` is NOT used when validating JWTs, the `aud` claim is used and defaults to `api://default`, if you change this you MUST change this on your Okta Authorization Server (via the admin console), and set the equivalent `okta.oauth2.audience` property to the same value

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @RequestMapping(value = "/data", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-    public ResponseEntity<String> getDemoData(OAuth2Authentication user) {
-        return new ResponseEntity<String>("Processed Request", HttpStatus.OK);
+    public ResponseEntity<String> getDemoData(JwtAuthenticationToken token) { // you could also just inject a simple 'Principal' here
+        return new ResponseEntity<String>("Processed Request: "+ token.getTokenAttributes(), HttpStatus.OK);
     }
 }
